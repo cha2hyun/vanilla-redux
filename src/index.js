d9f3983e119b104dbad3dfb1d4ad66c35dbec27c
cha2hyun-dev <cha2hyun.dev@gmail.com>
2020-09-10 17:54:51 +0900

    #1.1 Store and Reducer

--- src/index.js
+++ src/index.js
@@ -1,32 +1,38 @@
+import { createStore } from "redux";
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const clear = document.getElementById("clear");
-
 const number = document.querySelector("span");
-let count = 0;
 
-const updateText = () => {
-    number.innerText = count;
+const countModifier = (state = 0) => {
+    return "helo";
 };
+const countStore = createStore(countModifier);
+console.log(countStore.getState());
+// let count = 0;
+// const updateText = () => {
+//     number.innerText = count;
+// };
 
-const handleAdd = () => {
-    console.log("add");
-    count = count + 1;
-    updateText();
-};
+// const handleAdd = () => {
+//     console.log("add");
+//     count = count + 1;
+//     updateText();
+// };
 
-const handleMinus = () => {
-    console.log("minus");
-    count = count - 1;
-    updateText();
-};
+// const handleMinus = () => {
+//     console.log("minus");
+//     count = count - 1;
+//     updateText();
+// };
 
-const handleClear = () => {
-    console.log("clear");
-    count = 0;
-    updateText();
-};
+// const handleClear = () => {
+//     console.log("clear");
+//     count = 0;
+//     updateText();
+// };
 
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
-clear.addEventListener("click", handleClear);
+// add.addEventListener("click", handleAdd);
+// minus.addEventListener("click", handleMinus);
+// clear.addEventListener("click", handleClear);
