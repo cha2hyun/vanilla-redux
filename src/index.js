18d536427f58e2fff9ba19556cbb42d243ca2cc9
cha2hyun-dev <cha2hyun.dev@gmail.com>
2020-09-10 18:09:58 +0900

    #.13 Subscriptions

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import { createStore } from "redux";
 
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
-const clear = document.getElementById("clear");
 const number = document.querySelector("span");
+
+number.innerText = 0;
 
 const countModifier = (count = 0, action) => {
     if (action.type === "ADD") {
         return count + 1;
-    } else if (action.tpye === "MINUS") {
+    } else if (action.type === "MINUS") {
         return count - 1;
     } else {
         return count;
@@ -16,12 +17,19 @@ const countModifier = (count = 0, action) => {
 };
 const countStore = createStore(countModifier);
 
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "MINUS" });
-console.log(countStore.getState());
+const onChange = () => {
+    number.innerText = countStore.getState();
+};
+
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+    countStore.dispatch({ type: "ADD" });
+};
+
+const handleMinus = () => {
+    countStore.dispatch({ type: "MINUS" });
+};
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
