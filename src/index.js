6b4391b8893039e86f14a67d8973e1a8488b5d79
cha2hyun-dev <cha2hyun.dev@gmail.com>
2020-09-10 18:02:38 +0900

    #1.2 Actions

--- src/index.js
+++ src/index.js
@@ -5,34 +5,23 @@ const minus = document.getElementById("minus");
 const clear = document.getElementById("clear");
 const number = document.querySelector("span");
 
-const countModifier = (state = 0) => {
-    return "helo";
+const countModifier = (count = 0, action) => {
+    if (action.type === "ADD") {
+        return count + 1;
+    } else if (action.tpye === "MINUS") {
+        return count - 1;
+    } else {
+        return count;
+    }
 };
 const countStore = createStore(countModifier);
+
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "MINUS" });
 console.log(countStore.getState());
-// let count = 0;
-// const updateText = () => {
-//     number.innerText = count;
-// };
-
-// const handleAdd = () => {
-//     console.log("add");
-//     count = count + 1;
-//     updateText();
-// };
-
-// const handleMinus = () => {
-//     console.log("minus");
-//     count = count - 1;
-//     updateText();
-// };
-
-// const handleClear = () => {
-//     console.log("clear");
-//     count = 0;
-//     updateText();
-// };
-
-// add.addEventListener("click", handleAdd);
-// minus.addEventListener("click", handleMinus);
-// clear.addEventListener("click", handleClear);
